df59ec9397f6b30b180c825c5d6475b860500e08
Ashar Farooq <ashar744@gmail.com>
2021-07-15 00:48:32 -0400

    added plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
 
     <Layout title="Home Page">
-      <h3>My Blog</h3>    
+      <h3>Welcome to my blog!</h3>   
+      <StaticImage src = "../images/ashar.jpg" alt ="picture of Ashar Farooq"/> 
     </Layout>
   )
 }
